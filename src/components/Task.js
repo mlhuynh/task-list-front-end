4eebbadd7a9a15f3a5f3464902bf74d51d8efdea
mlhuynh <greenlinh@gmail.com>
2023-06-23 11:27:54 -0700

    Updated toggleButton and  deleteTask functions and PropTypes.

--- src/components/Task.js
+++ src/components/Task.js
@@ -7,22 +7,33 @@ const Task = (props) => {
   const [complete, setComplete] = useState(isComplete);
   const buttonClass = complete ? 'tasks__item__toggle--completed' : 'task__item__toggle--setComplete';
 
+  // const toggleButton = () => {
+  //   console.log("the toggle has been TOGGLED");
+  //   props.updateComplete(props.id);
+  // };
+
   const toggleButton = () => {
-    console.log("the toggle has been TOGGLED");
-    props.updateComplete(props.id);
+    console.log('the toggle has been TOGGLED');
+    props.onClickCallback(props.id);
   };
 
+  // const deleteTask = () => {
+  //   console.log("Task deleted successfully");
+  //   props.updateDelete(props.id);
+  // };
+
   const deleteTask = () => {
-    console.log("Task deleted successfully");
-    props.updateDelete(props.id);
+    console.log('Task deleted successfully');
+    props.deleteTaskCallback(props.id);
   };
 
   return (
     <li className="tasks__item">
       <button>
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
-        {title}
+        {/* onClick={() => setComplete(!complete)} */}
+        onClick={toggleButton}
+        {props.title}
       </button>
       <button className="tasks__item__remove button" onClick={deleteTask}>x</button>
     </li>
@@ -33,8 +44,10 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,  
-  updateComplete: PropTypes.func,
-  updateDelete: PropTypes.func
+  // updateComplete: PropTypes.func,
+  // updateDelete: PropTypes.func,
+  onClickCallback: PropTypes.func.isRequired,
+  deleteTaskCallback: PropTypes.func.isRequired
 };
 
 export default Task;
