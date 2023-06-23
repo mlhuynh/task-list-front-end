9f59ff927ee752ce3baf855e58b52a643ebb60bd
Charlie <charliethemushi@gmail.com>
2023-06-23 14:34:36 -0700

    created get pth to connect db with axios

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
-import React from 'react';
+import React, { useEffect} from 'react';
+import { useState } from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
+import axios from 'axios';
 
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+// const TASKS = [
+//   {
+//     id: 1,
+//     title: 'Mow the lawn',
+//     isComplete: false,
+//   },
+//   {
+//     id: 2,
+//     title: 'Cook Pasta',
+//     isComplete: true,
+//   },
+// ];
 
-const App = () => {
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const loadTask = () => {
+    axios
+    .get('https://task-list-api-c17.onrender.com/tasks')
+    .then((response) => {
+      const initialTaskData = [];
+      response.data.forEach((task) => {
+        initialTaskData.push(task);
+      });
+      setTasks(initialTaskData);
+    })
+    .catch((error) => {
+      console.log('le error', error);
+    });
+  }
+
+  useEffect(() => {
+    loadTask();
+  }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div><TaskList tasks={TASKS} /></div>
+        <div><TaskList tasks={tasks} /></div>
       </main>
     </div>
   );
-};
+}
 
 export default App;
